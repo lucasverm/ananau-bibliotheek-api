dc600ccc4a5d1626b85bfb486af9e15146963441
lucasverm <lucasvermeulen@gmail.com>
2020-03-09 14:01:49 -0500

    add Items (vergeten vorige commit)

---
+++ ananauAPI/Models/Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ananauAPI.Models
+{
+    public class Item
+    {
+
+        private string _naam;
+        public string Id { get; set; }
+        public string Naam
+        {
+            get
+            {
+                return _naam;
+            }
+            set
+            {            
+                _naam = value;
+            }
+        }
+        public List<GebruikerItem> Gebruikers { get; set; }
+
+        public Item()
+        {
+        }
+
+        public Item(string naam) : this()
+        {
+            Naam = naam;
+        }
+    }
+}
